bce623ec7425826b3707b5470d27923ba27c8dd8
shelepuginivan <shelepuginivanm@gmail.com>
2024-06-10 23:59:09 +0300

    feat(components): add component `Footer`

---
+++ components/Footer/index.tsx
@@ -0,0 +1,29 @@
+'use client'
+
+import Image from 'next/image'
+import { FC } from 'react'
+
+import cat from '@/assets/cat.svg'
+
+import styles from './styles.module.scss'
+
+export const Footer: FC = () => {
+    const catClick = () =>
+        window.open(
+            'https://drive.google.com/file/d/1nkVrF4CewV3TWAls6-qcnPLDVCacmB5U/view',
+        )
+
+    return (
+        <footer className={styles.footer}>
+            <button className={styles.catButton} onClick={catClick}>
+                <Image
+                    src={cat.src}
+                    width={78}
+                    height={36}
+                    alt="Спящий котик"
+                />
+            </button>
+            <p>Скотников Вадим Борисович, {new Date().getFullYear()}</p>
+        </footer>
+    )
+}
