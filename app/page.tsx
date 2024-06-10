bf659e423ccfbec1e43f9d49f4510e00d43d5da6
shelepuginivan <shelepuginivanm@gmail.com>
2024-06-11 00:07:57 +0300

    feat(app): add content to the home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,58 @@
+import Link from 'next/link'
+
+import anonForm from '@/assets/anon-form.png'
+import favicon from '@/assets/favicon.png'
+import raptor from '@/assets/raptor.png'
+import { Card } from '@/components/Card'
+import { Container } from '@/components/Container'
+import { HomepageNavigation } from '@/components/HomepageNavigation'
+
 import styles from './page.module.scss'
 
 export default function Home() {
     return (
         <main>
-            <h1 className={styles.heading}>Hello, world!</h1>
+            <Container>
+                <Card image={favicon} imageAlt="Логотип SVB369.ru">
+                    <h1>Добро пожаловать!</h1>
+                    <p>
+                        Здесь вы можете узнать информацию об олимпиадах, скачать
+                        полезные материалы, найти ссылки на интересные сайты,
+                        программы и статьи. А также можете найти секретные
+                        послания.
+                    </p>
+                </Card>
+            </Container>
+            <HomepageNavigation />
+            <Container>
+                <Link
+                    className={styles.linkCard}
+                    href="https://forms.gle/kkavKTSN86r3yZLx6"
+                    target="_blank"
+                    draggable={false}
+                >
+                    <Card
+                        image={anonForm}
+                        imageAlt="Ждун"
+                        imagePosition="right"
+                    >
+                        <h2>Анонимная форма</h2>
+                        <p>Напиши мне своё анонимное мнение.</p>
+                        <p>Напиши мне то, что не можешь сказать вслух.</p>
+                    </Card>
+                </Link>
+                <Link
+                    className={styles.linkCard}
+                    href="/secrets"
+                    draggable={false}
+                >
+                    <Card image={raptor} imageAlt="Раптор">
+                        <h2>Пасхалки</h2>
+                        <p>Если это понятно, то это понятно.</p>
+                        <p>Если это не понятно, то это не понятно.</p>
+                    </Card>
+                </Link>
+            </Container>
         </main>
     )
 }
