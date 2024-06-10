336cb40172924bea59b4498f22ecae4c84004c3f
shelepuginivan <shelepuginivanm@gmail.com>
2024-06-10 22:11:34 +0300

    feat(components): add component `HomepageNavigation`

---
+++ components/HomepageNavigation/index.tsx
@@ -0,0 +1,37 @@
+import { NotebookPen, Star, User } from 'lucide-react'
+import Link from 'next/link'
+import { FC } from 'react'
+
+import { Container } from '@/components/Container'
+
+import styles from './styles.module.scss'
+
+export const HomepageNavigation: FC = () => {
+    return (
+        <Container>
+            <h2>Навигация</h2>
+            <div className={styles.navigation}>
+                <nav className={styles.group}>
+                    <NotebookPen color="aquamarine" />
+                    <Link href="/materials/lessons">Материалы с урока</Link>
+                    <Link href="/schedule">Расписание</Link>
+                    <Link href="/exams">Экзамены</Link>
+                    <Link href="/olymp">Олимпиады</Link>
+                </nav>
+                <nav className={styles.group}>
+                    <Star color="yellow" />
+                    <Link href="/misc/articles">Статьи</Link>
+                    <Link href="/misc/programs">Программы</Link>
+                    <Link href="/misc/videos">Видео</Link>
+                    <Link href="/misc/websites">Сайты</Link>
+                    <Link href="/materials/images">Картинки</Link>
+                </nav>
+                <nav className={styles.group}>
+                    <User color="orchid" />
+                    <Link href="/about">О себе</Link>
+                    <Link href="/achievements">Достижения</Link>
+                </nav>
+            </div>
+        </Container>
+    )
+}
