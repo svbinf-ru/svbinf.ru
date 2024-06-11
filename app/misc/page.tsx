7da2ce72d766fd35616331804663fe9d63844f3b
shelepuginivan <shelepuginivanm@gmail.com>
2024-06-11 20:27:11 +0300

    feat(app): add content to the `/misc` page

--- app/misc/page.tsx
+++ app/misc/page.tsx
@@ -1,7 +1,42 @@
+import articles from '@/assets/misc/articles.webp'
+import programs from '@/assets/misc/programs.webp'
+import videos from '@/assets/misc/videos.webp'
+import websites from '@/assets/misc/websites.webp'
+import { Breadcrumbs } from '@/components/Breadcrumbs'
+import { Container } from '@/components/Container'
+import { Grid } from '@/components/Grid'
+import { NavigationBanner } from '@/components/NavigationBanner'
+
 export default function Miscellaneous() {
     return (
         <main>
-            <h1>Разное</h1>
+            <Breadcrumbs items={[{ href: '/misc', label: 'Разное' }]} />
+            <Container>
+                <h1>Разное</h1>
+                <p>Подборка полезных материалов.</p>
+                <Grid>
+                    <NavigationBanner
+                        image={videos}
+                        href="/misc/videos"
+                        title="Видео"
+                    />
+                    <NavigationBanner
+                        image={programs}
+                        href="/misc/programs"
+                        title="Программы"
+                    />
+                    <NavigationBanner
+                        image={websites}
+                        href="/misc/websites"
+                        title="Сайты"
+                    />
+                    <NavigationBanner
+                        image={articles}
+                        href="/misc/articles"
+                        title="Статьи"
+                    />
+                </Grid>
+            </Container>
         </main>
     )
 }
