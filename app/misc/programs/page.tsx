030e732b9675dcb32da3ffd0d86f4702b06eaa86
shelepuginivan <shelepuginivanm@gmail.com>
2024-06-11 17:05:34 +0300

    feat(app): add content to the `/misc/programs` page

--- app/misc/programs/page.tsx
+++ app/misc/programs/page.tsx
@@ -1,7 +1,32 @@
+import { Breadcrumbs } from '@/components/Breadcrumbs'
+import { Container } from '@/components/Container'
+import { Grid } from '@/components/Grid'
+import { Miscellaneous } from '@/components/Miscellaneous'
+import { programs } from '@/content/misc/programs'
+
 export default function Programs() {
     return (
         <main>
-            <h1>Программы</h1>
+            <Breadcrumbs
+                items={[
+                    { href: '/misc', label: 'Разное' },
+                    { href: '/misc/programs', label: 'Программы' },
+                ]}
+            />
+            <Container>
+                <h1>Программы</h1>
+                <Grid>
+                    {programs.map((p) => (
+                        <Miscellaneous
+                            key={p.title}
+                            href={p.href}
+                            thumbnail={p.thumbnail}
+                            title={p.title}
+                            description={p.description}
+                        />
+                    ))}
+                </Grid>
+            </Container>
         </main>
     )
 }
---
+++ content/misc/programs.ts
@@ -0,0 +1,57 @@
+import desktopGoose from '@/assets/misc/programs/desktop-goose.webp'
+import dosbox from '@/assets/misc/programs/dosbox.webp'
+import gimp from '@/assets/misc/programs/gimp.webp'
+import inkscape from '@/assets/misc/programs/inkscape.webp'
+import mailspring from '@/assets/misc/programs/mailspring.webp'
+import obsidian from '@/assets/misc/programs/obsidian.webp'
+import sumatrapdf from '@/assets/misc/programs/sumatrapdf.webp'
+
+import { Misc } from './interface'
+
+export const programs: Misc[] = [
+    {
+        href: 'https://www.getmailspring.com/',
+        thumbnail: mailspring,
+        title: 'Mailspring',
+        description: 'Быстрый, удобный и кастомизируемый почтовый клиент.',
+    },
+    {
+        href: 'https://www.gimp.org/',
+        thumbnail: gimp,
+        title: 'GIMP',
+        description:
+            'Бесплатный редактор растровой графики с открытым исходным кодом.',
+    },
+    {
+        href: 'https://inkscape.org/',
+        thumbnail: inkscape,
+        title: 'Inkscape',
+        description:
+            'Бесплатный редактор векторной графики с открытым исходным кодом.',
+    },
+    {
+        href: 'https://obsidian.md/',
+        thumbnail: obsidian,
+        title: 'Obsidian',
+        description: 'Создавайте свою базу знаний из заметок.',
+    },
+    {
+        href: 'https://www.sumatrapdfreader.org/',
+        thumbnail: sumatrapdf,
+        title: 'Sumatra PDF',
+        description: 'Простой и производительный просмотрщик PDF-документов.',
+    },
+    {
+        href: 'https://samperson.itch.io/desktop-goose',
+        thumbnail: desktopGoose,
+        title: 'Desktop Goose',
+        description:
+            'Гусь схватит вашу мышь, оставит грязь на вашем экране, напишет вам сообщение, доставит вам мемы!',
+    },
+    {
+        href: 'https://www.dosbox.com/',
+        thumbnail: dosbox,
+        title: 'DOSBox',
+        description: 'Эмулятор, запускающий программы для DOS',
+    },
+]
