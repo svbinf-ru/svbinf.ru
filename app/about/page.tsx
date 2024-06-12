67c5996de711248e44fd94fba99f0b985d77f162
shelepuginivan <shelepuginivanm@gmail.com>
2024-06-13 01:26:20 +0300

    feat(app): add content to the `/about` page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,7 +1,73 @@
+import Link from 'next/link'
+
+import profile from '@/assets/profile.webp'
+import { Breadcrumbs } from '@/components/Breadcrumbs'
+import { Card } from '@/components/Card'
+import { ContactLink } from '@/components/ContactLink'
+import { Container } from '@/components/Container'
+import { contacts } from '@/content/about/contacts'
+import { education } from '@/content/about/education'
+
 export default function About() {
     return (
         <main>
-            <h1>О себе</h1>
+            <Breadcrumbs items={[{ href: '/about', label: 'О себе' }]} />
+            <Container>
+                <Card
+                    image={profile}
+                    imageAlt="Скотников Вадим Борисович"
+                    imageWidth={256}
+                    imageHeight={256}
+                >
+                    <h1>Скотников Вадим Борисович</h1>
+                    <p>
+                        Учитель информатики, педагогический стаж работы 4 года.
+                    </p>
+                    <p>
+                        Установлена{' '}
+                        <Link
+                            href="/static/doc/об-установлении-первой-квалификационной-категории.pdf"
+                            target="_blank"
+                        >
+                            <b>первая квалификационная категория</b>
+                        </Link>{' '}
+                        с 17 декабря 2020.
+                    </p>
+                    <p>
+                        Установлена{' '}
+                        <Link
+                            href="/static/doc/об-установлении-высшей-квалификационной-категории.pdf"
+                            target="_blank"
+                        >
+                            <b>высшая квалификационная категория</b>
+                        </Link>{' '}
+                        с 26 января 2023.
+                    </p>
+                    <Container>
+                        {contacts.map((c) => (
+                            <ContactLink
+                                key={c.href}
+                                href={c.href}
+                                icon={c.icon}
+                                text={c.text}
+                            />
+                        ))}
+                    </Container>
+                </Card>
+                {education.map((e, i) => (
+                    <Card
+                        key={e.title}
+                        image={e.thumbnail}
+                        imageAlt={e.title}
+                        imagePosition={i % 2 == 0 ? 'left' : 'right'}
+                    >
+                        <Link target="_blank" href={e.full}>
+                            <h2>{e.title}</h2>
+                        </Link>
+                        <p>{e.description}</p>
+                    </Card>
+                ))}
+            </Container>
         </main>
     )
 }
