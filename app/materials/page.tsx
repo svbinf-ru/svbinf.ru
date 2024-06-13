0fb89ff776679dab28f0205ec26609d787c400d4
shelepuginivan <shelepuginivanm@gmail.com>
2024-06-13 16:39:28 +0300

    feat(app): add content to the `/materials` page

--- app/materials/page.tsx
+++ app/materials/page.tsx
@@ -1,7 +1,30 @@
+import images from '@/assets/materials/images.webp'
+import lessons from '@/assets/materials/lessons.webp'
+import { Breadcrumbs } from '@/components/Breadcrumbs'
+import { Container } from '@/components/Container'
+import { Grid } from '@/components/Grid'
+import { NavigationBanner } from '@/components/NavigationBanner'
+
 export default function Materials() {
     return (
         <main>
-            <h1>Материалы</h1>
+            <Breadcrumbs items={[{ href: '/materials', label: 'Материалы' }]} />
+            <Container>
+                <h1>Материалы</h1>
+                <p>Полезная информация, источники и материалы.</p>
+                <Grid>
+                    <NavigationBanner
+                        image={lessons}
+                        href="/materials/lessons"
+                        title="Материалы с урока"
+                    />
+                    <NavigationBanner
+                        image={images}
+                        href="/materials/images"
+                        title="Картинки"
+                    />
+                </Grid>
+            </Container>
         </main>
     )
 }
