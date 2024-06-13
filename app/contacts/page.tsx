558ef92d7d61d4e3397642ed5d23673065edf744
shelepuginivan <shelepuginivanm@gmail.com>
2024-06-13 16:52:15 +0300

    feat(app): add content to the `/contacts` page

--- app/contacts/page.tsx
+++ app/contacts/page.tsx
@@ -1,7 +1,32 @@
+import profile from '@/assets/profile.webp'
+import { Card } from '@/components/Card'
+import { ContactLink } from '@/components/ContactLink'
+import { Container } from '@/components/Container'
+import { contacts } from '@/content/contacts/contacts'
+
 export default function Contacts() {
     return (
         <main>
-            <h1>Контакты</h1>
+            <Container>
+                <Card
+                    image={profile}
+                    imageAlt="Скотников Вадим Борисович"
+                    imageWidth={256}
+                    imageHeight={256}
+                >
+                    <h1>Скотников Вадим Борисович</h1>
+                    <Container>
+                        {contacts.map((c) => (
+                            <ContactLink
+                                key={c.href}
+                                href={c.href}
+                                icon={c.icon}
+                                text={c.text}
+                            />
+                        ))}
+                    </Container>
+                </Card>
+            </Container>
         </main>
     )
 }
---
+++ content/contacts/contacts.ts
@@ -0,0 +1,20 @@
+import { Contact } from '@/content/utils'
+import { Mail, Phone, Send } from 'lucide-react'
+
+export const contacts: Contact[] = [
+    {
+        href: 'https://t.me/vadimbs_369',
+        icon: Send,
+        text: '@VadimBS_369',
+    },
+    {
+        href: 'mailto:skotnikovvb@licey369.ru',
+        icon: Mail,
+        text: 'SkotnikovVB@licey369.ru',
+    },
+    {
+        href: 'tel:+79117751614',
+        icon: Phone,
+        text: '+7 (911) 775-16-14',
+    },
+]
