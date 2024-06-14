d5cffdd31a699aeefbeeb02464ae47fd06c473ed
shelepuginivan <shelepuginivanm@gmail.com>
2024-06-14 18:15:20 +0300

    feat(app): add links to the `/olymp` page

--- app/olymp/page.tsx
+++ app/olymp/page.tsx
@@ -1,7 +1,29 @@
+import list from '@/assets/olymp/list.webp'
+import prep from '@/assets/olymp/prep.webp'
+import { Breadcrumbs } from '@/components/Breadcrumbs'
+import { Container } from '@/components/Container'
+import { Grid } from '@/components/Grid'
+import { NavigationBanner } from '@/components/NavigationBanner'
+
 export default function Olympiads() {
     return (
         <main>
-            <h1>Олимпиады</h1>
+            <Breadcrumbs items={[{ href: '/olymp', label: 'Олимпиады' }]} />
+            <Container>
+                <h1>Олимпиады</h1>
+                <Grid>
+                    <NavigationBanner
+                        href="/olymp/list"
+                        title="Олимпиады и конкурсы"
+                        image={list}
+                    />
+                    <NavigationBanner
+                        href="/olymp/prep"
+                        title="Ресурсы для подготовки"
+                        image={prep}
+                    />
+                </Grid>
+            </Container>
         </main>
     )
 }
