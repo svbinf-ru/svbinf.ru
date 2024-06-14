b0473fc7894f70cbaa24fbb8224f4cb088631a7a
shelepuginivan <shelepuginivanm@gmail.com>
2024-06-14 12:37:34 +0300

    build: setup `next-pwa`

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,14 @@
+import withPWAFactory from 'next-pwa'
+
+const withPWA = withPWAFactory({
+    dest: 'public',
+})
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+const nextConfig = withPWA({
     sassOptions: {
         includePaths: ['styles'],
     },
-}
+})
 
 export default nextConfig
