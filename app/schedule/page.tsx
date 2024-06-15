6372db737b12de621b71e0b959b567f9cb1c0eba
shelepuginivan <shelepuginivanm@gmail.com>
2024-06-15 14:20:04 +0300

    feat(app): add bell timetable to the `/schedule` page

--- app/schedule/page.tsx
+++ app/schedule/page.tsx
@@ -1,7 +1,36 @@
+import Link from 'next/link'
+
+import { BellTimetable } from '@/components/BellTimetable'
+import { Breadcrumbs } from '@/components/Breadcrumbs'
+import { Container } from '@/components/Container'
+
 export default function Schedule() {
     return (
         <main>
-            <h1>Расписание и звонки</h1>
+            <Breadcrumbs items={[{ href: '/schedule', label: 'Расписание' }]} />
+            <Container>
+                <h1>Расписание и звонки</h1>
+                <p>
+                    Скачать{' '}
+                    <Link
+                        href="/static/img/schedule/bell-timetable-vertical.png"
+                        target="_blank"
+                        download="расписание вертикальное.png"
+                    >
+                        вертикальное
+                    </Link>{' '}
+                    или{' '}
+                    <Link
+                        href="/static/img/schedule/bell-timetable-horizontal.png"
+                        target="_blank"
+                        download="расписание горизонтальное.png"
+                    >
+                        горизонтальное
+                    </Link>{' '}
+                    расписание звонков.
+                </p>
+                <BellTimetable />
+            </Container>
         </main>
     )
 }
