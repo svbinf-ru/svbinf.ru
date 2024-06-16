3a4abf4a6e0255620a437aa5e1067bacb21ec7df
shelepuginivan <shelepuginivanm@gmail.com>
2024-06-16 21:19:01 +0300

    feat(components): add component `Quotation`

---
+++ components/Quotation/index.tsx
@@ -0,0 +1,39 @@
+import Image, { StaticImageData } from 'next/image'
+import { FC } from 'react'
+
+import styles from './styles.module.scss'
+
+interface Props {
+    quote: string
+    author: string
+    picture: StaticImageData
+    pictureWidth?: number
+    pictureHeight?: number
+    unoptimized?: boolean
+}
+
+export const Quotation: FC<Props> = ({
+    quote,
+    author,
+    picture,
+    pictureWidth,
+    pictureHeight,
+    unoptimized = false,
+}) => {
+    return (
+        <blockquote className={styles.quotation}>
+            <Image
+                className={styles.image}
+                src={picture.src}
+                alt={author}
+                width={pictureWidth ?? picture.width}
+                height={pictureHeight ?? picture.height}
+                unoptimized={unoptimized}
+            />
+            <div className={styles.text}>
+                <p>{quote}</p>
+                <cite>{author}</cite>
+            </div>
+        </blockquote>
+    )
+}
