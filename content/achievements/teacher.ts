47b848770a10e7237b0f50d671289e4965a654dc
shelepuginivan <shelepuginivanm@gmail.com>
2024-06-21 22:44:12 +0300

    feat(content): add teacher achievements

---
+++ content/achievements/teacher.ts
@@ -0,0 +1,222 @@
+import teacherAchievement1 from '@/assets/achievements/teacher/2017 Б 45 лет района.webp'
+import teacherAchievement2 from '@/assets/achievements/teacher/2017 С КИТ.webp'
+import teacherAchievement3 from '@/assets/achievements/teacher/2018 С Бобёр.webp'
+import teacherAchievement4 from '@/assets/achievements/teacher/2019 Б Иллюстрация к сказке.webp'
+import teacherAchievement5 from '@/assets/achievements/teacher/2019 Б МХК.webp'
+import teacherAchievement6 from '@/assets/achievements/teacher/2019 Б Обществознание.webp'
+import teacherAchievement7 from '@/assets/achievements/teacher/2020 Г Конкурс сайтов.webp'
+import teacherAchievement8 from '@/assets/achievements/teacher/2020 С Жюри.webp'
+import teacherAchievement9 from '@/assets/achievements/teacher/2021 Б ННиБ(Б).webp'
+import teacherAchievement10 from '@/assets/achievements/teacher/2021 Б ННиБ(П).webp'
+import teacherAchievement11 from '@/assets/achievements/teacher/2021 С Pascal.webp'
+import teacherAchievement12 from '@/assets/achievements/teacher/2021 С Будущее с ии1.webp'
+import teacherAchievement13 from '@/assets/achievements/teacher/2021 С Будущее с ии2.webp'
+import teacherAchievement14 from '@/assets/achievements/teacher/2021 С НТО.webp'
+import teacherAchievement15 from '@/assets/achievements/teacher/2021 С СШ.webp'
+import teacherAchievement16 from '@/assets/achievements/teacher/2022 БП история.webp'
+import teacherAchievement17 from '@/assets/achievements/teacher/2022 Г Кр. Рна.webp'
+import teacherAchievement18 from '@/assets/achievements/teacher/2022 Д EDex.webp'
+import teacherAchievement19 from '@/assets/achievements/teacher/2022 Д ДвИ.webp'
+import teacherAchievement20 from '@/assets/achievements/teacher/2022 Д КС.webp'
+import teacherAchievement21 from '@/assets/achievements/teacher/2022 Д МЭО.webp'
+import teacherAchievement22 from '@/assets/achievements/teacher/2022 С УчиРу.webp'
+import teacherAchievement23 from '@/assets/achievements/teacher/2022 С ШИПУР ЧЖ.webp'
+import teacherAchievement24 from '@/assets/achievements/teacher/2023 Б НИ Б.webp'
+import teacherAchievement25 from '@/assets/achievements/teacher/2023 Б ЦТД С.webp'
+import teacherAchievement26 from '@/assets/achievements/teacher/2023 С Ru.webp'
+import teacherAchievement27 from '@/assets/achievements/teacher/2023 С ППЭ.webp'
+import teacherAchievement28 from '@/assets/achievements/teacher/2024 С ЕГЭ.webp'
+import teacherAchievement29 from '@/assets/achievements/teacher/2024 С ПМФ organizator.webp'
+import teacherAchievement30 from '@/assets/achievements/teacher/2024 С ПМФ spiker.webp'
+import teacherAchievement31 from '@/assets/achievements/teacher/2024 С ПМФ Сертификат.webp'
+
+import { Achievement } from '@/content/utils'
+
+export const teacher: Achievement[] = [
+    {
+        thumbnail: teacherAchievement31,
+        full: '/static/img/achievements/teacher/2024 С ПМФ Сертификат.jpg',
+        year: 2024,
+        type: 'certificate',
+    },
+    {
+        thumbnail: teacherAchievement30,
+        full: '/static/img/achievements/teacher/2024 С ПМФ spiker.jpg',
+        year: 2024,
+        type: 'certificate',
+    },
+    {
+        thumbnail: teacherAchievement29,
+        full: '/static/img/achievements/teacher/2024 С ПМФ organizator.jpg',
+        year: 2024,
+        type: 'certificate',
+    },
+    {
+        thumbnail: teacherAchievement28,
+        full: '/static/img/achievements/teacher/2024 С ЕГЭ.jpg',
+        year: 2024,
+        type: 'certificate',
+    },
+    {
+        thumbnail: teacherAchievement27,
+        full: '/static/img/achievements/teacher/2023 С ППЭ.jpg',
+        year: 2023,
+        type: 'certificate',
+    },
+    {
+        thumbnail: teacherAchievement26,
+        full: '/static/img/achievements/teacher/2023 С Ru.png',
+        year: 2023,
+        type: 'certificate',
+    },
+    {
+        thumbnail: teacherAchievement25,
+        full: '/static/img/achievements/teacher/2023 Б ЦТД С.jpg',
+        year: 2023,
+        type: 'gratitude',
+    },
+    {
+        thumbnail: teacherAchievement24,
+        full: '/static/img/achievements/teacher/2023 Б НИ Б.jpg',
+        year: 2023,
+        type: 'gratitude',
+    },
+    {
+        thumbnail: teacherAchievement23,
+        full: '/static/img/achievements/teacher/2022 С ШИПУР ЧЖ.jpg',
+        year: 2022,
+        type: 'certificate',
+    },
+    {
+        thumbnail: teacherAchievement22,
+        full: '/static/img/achievements/teacher/2022 С УчиРу.jpg',
+        year: 2022,
+        type: 'certificate',
+    },
+    {
+        thumbnail: teacherAchievement21,
+        full: '/static/img/achievements/teacher/2022 Д МЭО.jpg',
+        year: 2022,
+        type: 'diploma',
+    },
+    {
+        thumbnail: teacherAchievement20,
+        full: '/static/img/achievements/teacher/2022 Д КС.jpg',
+        year: 2022,
+        type: 'diploma',
+    },
+    {
+        thumbnail: teacherAchievement19,
+        full: '/static/img/achievements/teacher/2022 Д ДвИ.jpg',
+        year: 2022,
+        type: 'diploma',
+    },
+    {
+        thumbnail: teacherAchievement18,
+        full: '/static/img/achievements/teacher/2022 Д EDex.jpg',
+        year: 2022,
+        type: 'diploma',
+    },
+    {
+        thumbnail: teacherAchievement17,
+        full: '/static/img/achievements/teacher/2022 Г Кр. Рна.jpg',
+        year: 2022,
+        type: 'commendation',
+    },
+    {
+        thumbnail: teacherAchievement16,
+        full: '/static/img/achievements/teacher/2022 БП история.jpg',
+        year: 2022,
+        type: 'gratitude',
+    },
+    {
+        thumbnail: teacherAchievement15,
+        full: '/static/img/achievements/teacher/2021 С СШ.jpg',
+        year: 2021,
+        type: 'certificate',
+    },
+    {
+        thumbnail: teacherAchievement14,
+        full: '/static/img/achievements/teacher/2021 С НТО.jpg',
+        year: 2021,
+        type: 'certificate',
+    },
+    {
+        thumbnail: teacherAchievement13,
+        full: '/static/img/achievements/teacher/2021 С Будущее с ии2.png',
+        year: 2021,
+        type: 'certificate',
+    },
+    {
+        thumbnail: teacherAchievement12,
+        full: '/static/img/achievements/teacher/2021 С Будущее с ии1.png',
+        year: 2021,
+        type: 'certificate',
+    },
+    {
+        thumbnail: teacherAchievement11,
+        full: '/static/img/achievements/teacher/2021 С Pascal.jpg',
+        year: 2021,
+        type: 'certificate',
+    },
+    {
+        thumbnail: teacherAchievement10,
+        full: '/static/img/achievements/teacher/2021 Б ННиБ(П).jpg',
+        year: 2021,
+        type: 'gratitude',
+    },
+    {
+        thumbnail: teacherAchievement9,
+        full: '/static/img/achievements/teacher/2021 Б ННиБ(Б).jpg',
+        year: 2021,
+        type: 'gratitude',
+    },
+    {
+        thumbnail: teacherAchievement8,
+        full: '/static/img/achievements/teacher/2020 С Жюри.jpg',
+        year: 2020,
+        type: 'certificate',
+    },
+    {
+        thumbnail: teacherAchievement7,
+        full: '/static/img/achievements/teacher/2020 Г Конкурс сайтов.jpg',
+        year: 2020,
+        type: 'commendation',
+    },
+    {
+        thumbnail: teacherAchievement6,
+        full: '/static/img/achievements/teacher/2019 Б Обществознание.jpg',
+        year: 2019,
+        type: 'gratitude',
+    },
+    {
+        thumbnail: teacherAchievement5,
+        full: '/static/img/achievements/teacher/2019 Б МХК.jpg',
+        year: 2019,
+        type: 'gratitude',
+    },
+    {
+        thumbnail: teacherAchievement4,
+        full: '/static/img/achievements/teacher/2019 Б Иллюстрация к сказке.jpg',
+        year: 2019,
+        type: 'gratitude',
+    },
+    {
+        thumbnail: teacherAchievement3,
+        full: '/static/img/achievements/teacher/2018 С Бобёр.jpg',
+        year: 2018,
+        type: 'certificate',
+    },
+    {
+        thumbnail: teacherAchievement2,
+        full: '/static/img/achievements/teacher/2017 С КИТ.jpg',
+        year: 2017,
+        type: 'certificate',
+    },
+    {
+        thumbnail: teacherAchievement1,
+        full: '/static/img/achievements/teacher/2017 Б 45 лет района.jpg',
+        year: 2017,
+        type: 'gratitude',
+    },
+]
