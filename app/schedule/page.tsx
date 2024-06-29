33889fab9e9f59e341ea64d481d30b214d33a3c7
shelepuginivan <shelepuginivanm@gmail.com>
2024-06-29 15:17:39 +0300

    feat(app): add secret to the `/schedule` page

--- app/schedule/page.tsx
+++ app/schedule/page.tsx
@@ -2,8 +2,10 @@ import { Metadata } from 'next'
 import Link from 'next/link'
 
 import linusTorvalds from '@/assets/quotes/linus-torvalds.webp'
+import consultations from '@/assets/schedule/consultations.webp'
 import { BellTimetable } from '@/components/BellTimetable'
 import { Breadcrumbs } from '@/components/Breadcrumbs'
+import { CardLink } from '@/components/CardLink'
 import { Container } from '@/components/Container'
 import { Quotation } from '@/components/Quotation'
 
@@ -37,6 +39,19 @@ export default function Schedule() {
                     расписание звонков.
                 </p>
                 <BellTimetable />
+                <CardLink
+                    href="https://xn--_____-4vebdbp0abgazhnfyk0cngb6a0ag7bf30bwa6b/"
+                    target="_blank"
+                    image={consultations}
+                    imageAlt="Консультации"
+                    imageWidth={256}
+                    imageHeight={256}
+                >
+                    <h2>Консультации</h2>
+                    <p>
+                        Консультации проходят по предворительной договорённости.
+                    </p>
+                </CardLink>
                 <Quotation
                     quote="Интеллект — это способность избегать выполнения работы, но так, чтобы она при этом была сделана."
                     author="Линус Торвальдс"
