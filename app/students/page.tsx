55ebbed2211b001bfc399dee7a1a0d4b17730c55
shelepuginivan <shelepuginivanm@gmail.com>
2024-06-29 12:07:48 +0300

    feat(app): add page `/students`

---
+++ app/students/page.tsx
@@ -0,0 +1,36 @@
+import { Metadata } from 'next'
+
+import { Breadcrumbs } from '@/components/Breadcrumbs'
+import { CardLink } from '@/components/CardLink'
+import { Container } from '@/components/Container'
+import { students } from '@/content/students/students'
+
+export const metadata: Metadata = {
+    title: 'Ученики',
+}
+
+export default function Secrets() {
+    return (
+        <main>
+            <Breadcrumbs items={[{ href: '/students', label: 'Ученики' }]} />
+            <Container>
+                <h1>Мои ученики</h1>
+                {students.map((s, i) => (
+                    <CardLink
+                        key={s.name}
+                        href={s.url}
+                        target="_blank"
+                        image={s.picture}
+                        imageAlt={s.name}
+                        imageWidth={256}
+                        imageHeight={256}
+                        imagePosition={i % 2 == 0 ? 'left' : 'right'}
+                    >
+                        <h2>{s.name}</h2>
+                        <p>{s.description}</p>
+                    </CardLink>
+                ))}
+            </Container>
+        </main>
+    )
+}
