03f2fb5ce3d224962a2d399ba047eaaa9daa18f6
shelepuginivan <shelepuginivanm@gmail.com>
2024-08-03 04:31:15 +0300

    feat(app): use grid layout and new component

--- app/exams/page.tsx
+++ app/exams/page.tsx
@@ -1,21 +1,17 @@
-import { ChevronsUpDown, ChevronUp, Info, Users } from 'lucide-react'
 import { Metadata } from 'next'
-import { Nunito } from 'next/font/google'
 
 import alanTuring from '@/assets/quotes/alan-turing.webp'
 import { Breadcrumbs } from '@/components/Breadcrumbs'
 import { Container } from '@/components/Container'
-import { ContainerBorders } from '@/components/ContainerBorders'
 import { Quotation } from '@/components/Quotation'
 import { results } from '@/content/exams/results'
 
-import styles from './styles.module.scss'
+import { Grid } from '@/components/Grid'
+import { ContainerExam } from '@/components/ContainerExam'
 
 export const metadata: Metadata = {
     title: 'Экзамены',
 }
-
-const nunito = Nunito({ subsets: ['cyrillic'] })
 
 export default function Exams() {
     return (
@@ -24,52 +20,19 @@ export default function Exams() {
             <Container>
                 <h1>Экзамены</h1>
                 <h2>Результаты прошлых лет</h2>
-                {results.map((r) => (
-                    <ContainerBorders key={r.title}>
-                        <h3 className={styles.resultTitle}>{r.title}</h3>
-                        <div className={nunito.className}>
-                            {r.students && (
-                                <p className={styles.entry}>
-                                    <Users />
-                                    <span>
-                                        Учеников: <b>{r.students}</b>
-                                    </span>
-                                </p>
-                            )}
-                            {r.average && (
-                                <p className={styles.entry}>
-                                    <ChevronsUpDown />
-                                    <span>
-                                        Средний:{' '}
-                                        <b>
-                                            {r.average}/{r.max}
-                                        </b>
-                                    </span>
-                                </p>
-                            )}
-                            {r.best && (
-                                <p className={styles.entry}>
-                                    <ChevronUp />
-                                    <span>
-                                        Лучший:{' '}
-                                        <b>
-                                            {r.best}/{r.max}
-                                        </b>
-                                    </span>
-                                </p>
-                            )}
-                        </div>
-                        <div>
-                            {r.info &&
-                                r.info.map((i) => (
-                                    <p className={styles.entry} key={i}>
-                                        <Info />
-                                        <span>{i}</span>
-                                    </p>
-                                ))}
-                        </div>
-                    </ContainerBorders>
-                ))}
+                <Grid>
+                    {results.map((r) => (
+                        <ContainerExam
+                            key={r.title}
+                            title={r.title}
+                            max={r.max}
+                            students={r.students}
+                            average={r.average}
+                            best={r.best}
+                            info={r.info}
+                        />
+                    ))}
+                </Grid>
                 <Quotation
                     quote="Те, кто могут что-то представить, способны сотворить невозможное."
                     author="Алан Тьюринг"
