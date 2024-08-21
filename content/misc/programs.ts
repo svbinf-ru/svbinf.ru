87bfd42fbcf92d072d7171f0951cc2cce6fadc36
shelepuginivan <shelepuginivanm@gmail.com>
2024-08-21 13:40:26 +0300

    chore(misc): add more programs
    Fixes #15

--- content/misc/programs.ts
+++ content/misc/programs.ts
@@ -1,8 +1,13 @@
+import alacritty from '@/assets/misc/programs/alacritty.webp'
+import blender from '@/assets/misc/programs/blender.webp'
 import desktopGoose from '@/assets/misc/programs/desktop-goose.webp'
 import dosbox from '@/assets/misc/programs/dosbox.webp'
 import gimp from '@/assets/misc/programs/gimp.webp'
+import hakutest from '@/assets/misc/programs/hakutest.webp'
 import inkscape from '@/assets/misc/programs/inkscape.webp'
+import keepassxc from '@/assets/misc/programs/keepassxc.webp'
 import mailspring from '@/assets/misc/programs/mailspring.webp'
+import obs from '@/assets/misc/programs/obs.webp'
 import obsidian from '@/assets/misc/programs/obsidian.webp'
 import sumatrapdf from '@/assets/misc/programs/sumatrapdf.webp'
 
@@ -10,10 +15,38 @@ import { Misc } from '@/content/utils'
 
 export const programs: Misc[] = [
     {
+        href: 'https://hakutest.org/ru/',
+        thumbnail: hakutest,
+        title: 'Hakutest',
+        description:
+            'Система для образовательного тестирования с автоматической проверкой ответов.',
+    },
+    {
+        href: 'https://alacritty.org/',
+        thumbnail: alacritty,
+        title: 'Alacritty',
+        description:
+            'Минималистичный и быстрый терминал с графическим ускорением.',
+    },
+    {
+        href: 'https://obsproject.com/',
+        thumbnail: obs,
+        title: 'OBS Studio',
+        description:
+            'Бесплатное ПО для записи видео и трансляций с открытым исходным кодом.',
+    },
+    {
         href: 'https://www.getmailspring.com/',
         thumbnail: mailspring,
         title: 'Mailspring',
         description: 'Быстрый, удобный и кастомизируемый почтовый клиент.',
+    },
+    {
+        href: 'https://www.blender.org/',
+        thumbnail: blender,
+        title: 'Blender',
+        description:
+            'Бесплатный пакет для создания 3D-изображений с открытым исходным кодом.',
     },
     {
         href: 'https://www.gimp.org/',
@@ -28,6 +61,13 @@ export const programs: Misc[] = [
         title: 'Inkscape',
         description:
             'Бесплатный редактор векторной графики с открытым исходным кодом.',
+    },
+    {
+        href: 'https://keepassxc.org/',
+        thumbnail: keepassxc,
+        title: 'KeePassXC',
+        description:
+            'Удобный и безопасный менеджер пароль с множеством возможностей.',
     },
     {
         href: 'https://obsidian.md/',
