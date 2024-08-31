59827731a514885dc7bc725cc1f0659fb8ab8070
shelepuginivan <shelepuginivanm@gmail.com>
2024-08-31 23:04:06 +0300

    chore(olymp-list): two columns on large screens
    Fixes #17

--- app/olymp/list/page.tsx
+++ app/olymp/list/page.tsx
@@ -1,20 +1,14 @@
-import { Calendar, Info, MapPin, Star, Type } from 'lucide-react'
 import { Metadata } from 'next'
-import { Nunito } from 'next/font/google'
-import Link from 'next/link'
 
 import { Breadcrumbs } from '@/components/Breadcrumbs'
 import { Container } from '@/components/Container'
-import { ContainerBorders } from '@/components/ContainerBorders'
+import { ContainerOlymp } from '@/components/ContainerOlymp'
+import { GridTwoColumns } from '@/components/GridTwoColumns'
 import { olympiads } from '@/content/olymp/list'
-
-import styles from './styles.module.scss'
 
 export const metadata: Metadata = {
     title: 'Список олимпиад и конкурсов',
 }
-
-const nunito = Nunito({ subsets: ['cyrillic'] })
 
 export default function Olympiads() {
     return (
@@ -31,56 +25,19 @@ export default function Olympiads() {
                     Подборка олимпиад и конкурсов, в которых вы можете принять
                     участие.
                 </p>
-                {olympiads.map((r) => (
-                    <ContainerBorders key={r.title}>
-                        <Link href={r.href} target="_blank">
-                            <h3 className={styles.resultTitle}>{r.title}</h3>
-                        </Link>
-                        <div className={nunito.className}>
-                            {r.type && (
-                                <p className={styles.entry}>
-                                    <Type />
-                                    <span>
-                                        Тип: <b>{r.type}</b>
-                                    </span>
-                                </p>
-                            )}
-                            {r.level && (
-                                <p className={styles.entry}>
-                                    <Star />
-                                    <span>
-                                        Уровень: <b>{r.level}</b>
-                                    </span>
-                                </p>
-                            )}
-                            {r.date && (
-                                <p className={styles.entry}>
-                                    <Calendar />
-                                    <span>
-                                        Дата: <b>{r.date}</b>
-                                    </span>
-                                </p>
-                            )}
-                            {r.place && (
-                                <p className={styles.entry}>
-                                    <MapPin />
-                                    <span>
-                                        Место: <b>{r.place}</b>
-                                    </span>
-                                </p>
-                            )}
-                        </div>
-                        <div>
-                            {r.info &&
-                                r.info.map((i) => (
-                                    <p className={styles.entry} key={i}>
-                                        <Info />
-                                        <span>{i}</span>
-                                    </p>
-                                ))}
-                        </div>
-                    </ContainerBorders>
-                ))}
+                <GridTwoColumns>
+                    {olympiads.map((o) => (
+                        <ContainerOlymp
+                            key={o.title}
+                            title={o.title}
+                            href={o.href}
+                            level={o.level}
+                            date={o.date}
+                            place={o.place}
+                            info={o.info}
+                        />
+                    ))}
+                </GridTwoColumns>
                 {/* TODO: add quote */}
             </Container>
         </main>
---
+++ components/ContainerOlymp/index.tsx
@@ -0,0 +1,71 @@
+import { Calendar, Info, MapPin, Star, Type } from 'lucide-react'
+import { Nunito } from 'next/font/google'
+import Link from 'next/link'
+import { FC } from 'react'
+
+import { Olymp } from '@/content/olymp'
+
+import styles from './styles.module.scss'
+
+const nunito = Nunito({ subsets: ['cyrillic'] })
+
+export const ContainerOlymp: FC<Olymp> = ({
+    title,
+    href,
+    type,
+    level,
+    date,
+    place,
+    info,
+}) => {
+    return (
+        <div className={styles.container}>
+            <Link href={href} target="_blank">
+                <h3 className={styles.resultTitle}>{title}</h3>
+            </Link>
+            <div className={nunito.className}>
+                {type && (
+                    <p className={styles.entry}>
+                        <Type />
+                        <span>
+                            Тип: <b>{type}</b>
+                        </span>
+                    </p>
+                )}
+                {level && (
+                    <p className={styles.entry}>
+                        <Star />
+                        <span>
+                            Уровень: <b>{level}</b>
+                        </span>
+                    </p>
+                )}
+                {date && (
+                    <p className={styles.entry}>
+                        <Calendar />
+                        <span>
+                            Дата: <b>{date}</b>
+                        </span>
+                    </p>
+                )}
+                {place && (
+                    <p className={styles.entry}>
+                        <MapPin />
+                        <span>
+                            Место: <b>{place}</b>
+                        </span>
+                    </p>
+                )}
+            </div>
+            <div>
+                {info &&
+                    info.map((i) => (
+                        <p className={styles.entry} key={i}>
+                            <Info />
+                            <span>{i}</span>
+                        </p>
+                    ))}
+            </div>
+        </div>
+    )
+}
