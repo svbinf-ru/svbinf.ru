e1a943129b94c0b8a06c59af2d0a4e0552ca94a1
shelepuginivan <shelepuginivanm@gmail.com>
2024-10-12 16:04:50 +0300

    chore(materials): reorganize avatars

--- app/materials/images/page.tsx
+++ app/materials/images/page.tsx
@@ -3,6 +3,7 @@ import { Metadata } from 'next'
 import benjaminFranklin from '@/assets/quotes/benjamin-franklin.webp'
 import { Breadcrumbs } from '@/components/Breadcrumbs'
 import { Container } from '@/components/Container'
+import { ContainerBorders } from '@/components/ContainerBorders'
 import { ImageWithLink } from '@/components/ImageWithLink'
 import { Quotation } from '@/components/Quotation'
 import { Waterfall } from '@/components/Waterfall'
@@ -23,21 +24,33 @@ export default function Images() {
             />
             <Container>
                 <h1>Картинки</h1>
-                <h2>Аватары</h2>
-                <p>
-                    Важно сохранять анонимность, но можно добавить в это
-                    немножко стиля.
-                </p>
-                <Waterfall>
-                    {avatars.map((a) => (
-                        <ImageWithLink
-                            key={a.src.src}
-                            href={a.full}
-                            src={a.src}
-                            alt={a.caption}
-                        />
-                    ))}
-                </Waterfall>
+                <ContainerBorders>
+                    <h2>Аватарки</h2>
+                    <p>
+                        Важно сохранять анонимность, но можно добавить в это
+                        немножко стиля.
+                    </p>
+                    <b>
+                        Больше аватарок{' '}
+                        <a
+                            href="https://github.com/svb369-ru/avatars"
+                            target="_blank"
+                        >
+                            в репозитории GitHub
+                        </a>
+                        . Вы можете внести свой вклад!
+                    </b>
+                    <Waterfall>
+                        {avatars.map((a) => (
+                            <ImageWithLink
+                                key={a.src.src}
+                                href={a.full}
+                                src={a.src}
+                                alt={a.caption}
+                            />
+                        ))}
+                    </Waterfall>
+                </ContainerBorders>
                 <Quotation
                     quote="Скажи мне — и я забуду, учи меня — и я могу запомнить, вовлекай меня — и я научусь."
                     author="Бенджамин Франклин"
--- content/materials/images.ts
+++ content/materials/images.ts
@@ -1,221 +1,41 @@
-import avatar01 from '@/assets/materials/images/avatars/01.webp'
-import avatar02 from '@/assets/materials/images/avatars/02.webp'
-import avatar03 from '@/assets/materials/images/avatars/03.webp'
-import avatar04 from '@/assets/materials/images/avatars/04.webp'
-import avatar05 from '@/assets/materials/images/avatars/05.webp'
-import avatar06 from '@/assets/materials/images/avatars/06.webp'
-import avatar07 from '@/assets/materials/images/avatars/07.webp'
-import avatar09 from '@/assets/materials/images/avatars/09.webp'
-import avatar10 from '@/assets/materials/images/avatars/10.webp'
-import avatar11 from '@/assets/materials/images/avatars/11.webp'
-import avatar12 from '@/assets/materials/images/avatars/12.webp'
-import avatar13 from '@/assets/materials/images/avatars/13.webp'
-import avatar14 from '@/assets/materials/images/avatars/14.webp'
-import avatar15 from '@/assets/materials/images/avatars/15.webp'
-import avatar16 from '@/assets/materials/images/avatars/16.webp'
-import avatar17 from '@/assets/materials/images/avatars/17.webp'
-import avatar18 from '@/assets/materials/images/avatars/18.webp'
-import avatar19 from '@/assets/materials/images/avatars/19.webp'
-import avatar20 from '@/assets/materials/images/avatars/20.webp'
-import avatar21 from '@/assets/materials/images/avatars/21.webp'
-import avatar22 from '@/assets/materials/images/avatars/22.webp'
-import avatar23 from '@/assets/materials/images/avatars/23.webp'
-import avatar24 from '@/assets/materials/images/avatars/24.webp'
-import avatar25 from '@/assets/materials/images/avatars/25.webp'
-import avatar26 from '@/assets/materials/images/avatars/26.webp'
-import avatar27 from '@/assets/materials/images/avatars/27.webp'
-import avatar28 from '@/assets/materials/images/avatars/28.webp'
-import avatar29 from '@/assets/materials/images/avatars/29.webp'
-import avatar30 from '@/assets/materials/images/avatars/30.webp'
-import avatar31 from '@/assets/materials/images/avatars/31.webp'
-import avatar32 from '@/assets/materials/images/avatars/32.webp'
-import avatar33 from '@/assets/materials/images/avatars/33.webp'
-import avatar34 from '@/assets/materials/images/avatars/34.webp'
-import avatar35 from '@/assets/materials/images/avatars/35.webp'
-import avatar36 from '@/assets/materials/images/avatars/36.webp'
-import avatar37 from '@/assets/materials/images/avatars/37.webp'
+import avatar1 from '@/assets/materials/images/avatars/1.webp'
+import avatar2 from '@/assets/materials/images/avatars/2.webp'
+import avatar3 from '@/assets/materials/images/avatars/3.webp'
+import avatar4 from '@/assets/materials/images/avatars/4.webp'
+import avatar5 from '@/assets/materials/images/avatars/5.webp'
+import avatar6 from '@/assets/materials/images/avatars/6.webp'
 
 import { MaterialsImage } from '../utils'
 
 export const avatars: MaterialsImage[] = [
     {
-        src: avatar01,
+        src: avatar1,
         caption: 'Аватар 1',
         full: 'https://cdn.svb369.ru/i/avatars/01.jpg',
     },
     {
-        src: avatar02,
+        src: avatar2,
         caption: 'Аватар 2',
-        full: 'https://cdn.svb369.ru/i/avatars/02.jpg',
-    },
-    {
-        src: avatar03,
-        caption: 'Аватар 3',
-        full: 'https://cdn.svb369.ru/i/avatars/03.jpg',
-    },
-    {
-        src: avatar04,
-        caption: 'Аватар 4',
-        full: 'https://cdn.svb369.ru/i/avatars/04.jpg',
-    },
-    {
-        src: avatar05,
-        caption: 'Аватар 5',
         full: 'https://cdn.svb369.ru/i/avatars/05.jpg',
     },
     {
-        src: avatar06,
-        caption: 'Аватар 6',
-        full: 'https://cdn.svb369.ru/i/avatars/06.jpg',
-    },
-    {
-        src: avatar07,
-        caption: 'Аватар 7',
-        full: 'https://cdn.svb369.ru/i/avatars/07.jpg',
-    },
-    {
-        src: avatar09,
-        caption: 'Аватар 9',
-        full: 'https://cdn.svb369.ru/i/avatars/09.jpg',
-    },
-    {
-        src: avatar10,
-        caption: 'Аватар 10',
-        full: 'https://cdn.svb369.ru/i/avatars/10.jpg',
-    },
-    {
-        src: avatar11,
-        caption: 'Аватар 11',
-        full: 'https://cdn.svb369.ru/i/avatars/11.jpg',
-    },
-    {
-        src: avatar12,
-        caption: 'Аватар 12',
-        full: 'https://cdn.svb369.ru/i/avatars/12.jpg',
-    },
-    {
-        src: avatar13,
-        caption: 'Аватар 13',
-        full: 'https://cdn.svb369.ru/i/avatars/13.jpg',
-    },
-    {
-        src: avatar14,
-        caption: 'Аватар 14',
-        full: 'https://cdn.svb369.ru/i/avatars/14.jpg',
-    },
-    {
-        src: avatar15,
-        caption: 'Аватар 15',
-        full: 'https://cdn.svb369.ru/i/avatars/15.jpg',
-    },
-    {
-        src: avatar16,
-        caption: 'Аватар 16',
-        full: 'https://cdn.svb369.ru/i/avatars/16.jpg',
-    },
-    {
-        src: avatar17,
-        caption: 'Аватар 17',
-        full: 'https://cdn.svb369.ru/i/avatars/17.jpg',
-    },
-    {
-        src: avatar18,
-        caption: 'Аватар 18',
-        full: 'https://cdn.svb369.ru/i/avatars/18.jpg',
-    },
-    {
-        src: avatar19,
-        caption: 'Аватар 19',
-        full: 'https://cdn.svb369.ru/i/avatars/19.jpg',
-    },
-    {
-        src: avatar20,
-        caption: 'Аватар 20',
-        full: 'https://cdn.svb369.ru/i/avatars/20.jpg',
-    },
-    {
-        src: avatar21,
-        caption: 'Аватар 21',
-        full: 'https://cdn.svb369.ru/i/avatars/21.jpg',
-    },
-    {
-        src: avatar22,
-        caption: 'Аватар 22',
-        full: 'https://cdn.svb369.ru/i/avatars/22.jpg',
-    },
-    {
-        src: avatar23,
-        caption: 'Аватар 23',
-        full: 'https://cdn.svb369.ru/i/avatars/23.jpg',
-    },
-    {
-        src: avatar24,
-        caption: 'Аватар 24',
-        full: 'https://cdn.svb369.ru/i/avatars/24.jpg',
-    },
-    {
-        src: avatar25,
-        caption: 'Аватар 25',
-        full: 'https://cdn.svb369.ru/i/avatars/25.jpg',
-    },
-    {
-        src: avatar26,
-        caption: 'Аватар 26',
-        full: 'https://cdn.svb369.ru/i/avatars/26.jpg',
-    },
-    {
-        src: avatar27,
-        caption: 'Аватар 27',
-        full: 'https://cdn.svb369.ru/i/avatars/27.jpg',
-    },
-    {
-        src: avatar28,
-        caption: 'Аватар 28',
-        full: 'https://cdn.svb369.ru/i/avatars/28.png',
-    },
-    {
-        src: avatar29,
-        caption: 'Аватар 29',
-        full: 'https://cdn.svb369.ru/i/avatars/29.png',
-    },
-    {
-        src: avatar30,
-        caption: 'Аватар 30',
-        full: 'https://cdn.svb369.ru/i/avatars/30.png',
-    },
-    {
-        src: avatar31,
-        caption: 'Аватар 31',
+        src: avatar5,
+        caption: 'Аватар 3',
         full: 'https://cdn.svb369.ru/i/avatars/31.png',
     },
     {
-        src: avatar32,
-        caption: 'Аватар 32',
-        full: 'https://cdn.svb369.ru/i/avatars/32.png',
+        src: avatar3,
+        caption: 'Аватар 4',
+        full: 'https://cdn.svb369.ru/i/avatars/17.jpg',
     },
     {
-        src: avatar33,
-        caption: 'Аватар 33',
+        src: avatar4,
+        caption: 'Аватар 5',
+        full: 'https://cdn.svb369.ru/i/avatars/22.jpg',
+    },
+    {
+        src: avatar6,
+        caption: 'Аватар 6',
         full: 'https://cdn.svb369.ru/i/avatars/33.png',
-    },
-    {
-        src: avatar34,
-        caption: 'Аватар 34',
-        full: 'https://cdn.svb369.ru/i/avatars/34.png',
-    },
-    {
-        src: avatar35,
-        caption: 'Аватар 35',
-        full: 'https://cdn.svb369.ru/i/avatars/35.png',
-    },
-    {
-        src: avatar36,
-        caption: 'Аватар 36',
-        full: 'https://cdn.svb369.ru/i/avatars/36.png',
-    },
-    {
-        src: avatar37,
-        caption: 'Аватар 37',
-        full: 'https://cdn.svb369.ru/i/avatars/37.png',
     },
 ]
