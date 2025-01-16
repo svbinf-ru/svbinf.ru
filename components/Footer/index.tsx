f220cac13ceb6f9dabbb67bf32f6d24c4318c3a1
shelepuginivan <shelepuginivanm@gmail.com>
2025-01-17 01:43:25 +0300

    chore: link to telegram
    Closes #25

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import { Send } from 'lucide-react'
 import Image from 'next/image'
 import { FC } from 'react'
 
@@ -16,14 +17,24 @@ const catClick = () => {
 export const Footer: FC = () => {
     return (
         <footer id="footer" className={styles.footer}>
-            <button className={styles.catButton} onClick={catClick}>
-                <Image
-                    src={cat.src}
-                    width={78}
-                    height={36}
-                    alt="Спящий котик"
-                />
-            </button>
+            <div className={styles.buttonWrapper}>
+                <button className={styles.catButton} onClick={catClick}>
+                    <Image
+                        src={cat.src}
+                        width={78}
+                        height={36}
+                        alt="Спящий котик"
+                    />
+                </button>
+                <a href="https://t.me/VadimBS_369" target="_blank">
+                    <Send
+                        color="white"
+                        width={30}
+                        height={30}
+                        strokeWidth={1}
+                    />
+                </a>
+            </div>
             <p>Скотников Вадим Борисович, {new Date().getFullYear()}</p>
         </footer>
     )
